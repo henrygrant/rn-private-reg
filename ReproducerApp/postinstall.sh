901231731566c33c8ee6127c3e89db49a2e76e8b
Henry Grant <thomas.grant@ally.com>
2024-09-13 10:56:29 -0400

    feat: reconfigure per cortinico feedback

--- ReproducerApp/postinstall.sh
+++ ReproducerApp/postinstall.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-
-# macos syntax
-echo "running postinstall"
-sed -i '' '10i\
-    maven { url = uri(\"https://my-repo.com/repo\") }\
+if grep -q nexus "node_modules/@react-native/gradle-plugin/settings.gradle.kts"; then true; else
+    sed -i '' '10i\
+    maven { url = uri(\"https://myrepo.com/nexus/repository\") }\
 ' node_modules/@react-native/gradle-plugin/settings.gradle.kts
+    sed -i '' '1i\
+dependencyResolutionManagement {\
+    repositories {\
+        clear()\
+        maven { url = uri(\"https://myrepo.com/nexus/repository\") }\
+    }\
+}\
+' node_modules/@react-native/gradle-plugin/settings.gradle.kts
+fi
